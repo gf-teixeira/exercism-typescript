d36f518c84a5ac31ec91e95beba16234b17c59fa
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-11-06 22:26:10 -0300

    word-count

--- word-count/word-count.ts
+++ word-count/word-count.ts
@@ -2,9 +2,8 @@ export function count(sentence: string) {
 	let splitedSentence = sentence.replace(/\s+/g, ' ').trim().toLowerCase().split(' ');
 	let map = new Map();
 	splitedSentence.forEach((word) => {
-		let aux = map.get(word);
-		if(aux) aux +=1;
-		map.set(word, aux || 1);
+		map.set(word, map.get(word)+1 || 1);
+
 	})
 	return map
 }
