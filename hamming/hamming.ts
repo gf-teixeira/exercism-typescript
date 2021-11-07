8e1a8891a83a1651c4e3f622370093e85db09654
gf-teixeira <gabriel.fteixeira13@gmail.com>
2021-11-06 21:16:48 -0300

    hamming

---
+++ hamming/hamming.ts
@@ -0,0 +1,14 @@
+export function compute(left: string, right: string): number{
+
+	if(left.length !== right.length) throw new Error('DNA strands must be of equal length.');
+	
+	const leftStrand = [...left];
+	const rightStrand = [...right];
+	let cout = 0;
+
+	for(let i=0; i<left.length; i++){
+		if(leftStrand[i] !== rightStrand[i]) cout++;
+	}
+	return cout;
+
+}
